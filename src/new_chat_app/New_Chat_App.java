f51f5256bd005b94ec28baf67d4720282aa6676c
zakkib <zzakkibaldani@gmail.com>
2024-06-17 15:32:08 +0700

    First Commit

---
+++ src/new_chat_app/New_Chat_App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package new_chat_app;
+
+/**
+ *
+ * @author Zakki
+ */
+public class New_Chat_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
