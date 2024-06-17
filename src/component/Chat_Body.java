0ad6f887c95018a4d967611d1491e5cf42e6f1f9
zakkib <zzakkibaldani@gmail.com>
2024-06-17 17:11:05 +0700

    added component package and some classes

---
+++ src/component/Chat_Body.java
@@ -0,0 +1,94 @@
+
+package component;
+
+import java.awt.Color;
+import javax.swing.JScrollBar;
+
+/**
+ *
+ * @author Zakki
+ */
+public class Chat_Body extends javax.swing.JPanel {
+
+    public Chat_Body() {
+        initComponents();
+        init();
+    }
+
+private void init(){
+        body.setLayout(new MigLayout("fillx"));
+        sp.setVerticalScrollBar(new JScrollBar());
+        sp.getVerticalScrollBar().setBackground(Color.WHITE);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        sp = new javax.swing.JScrollPane();
+        body = new javax.swing.JPanel();
+        chatTitle = new component.Chat_Title();
+        chatBottom = new component.Chat_Bottom();
+
+        sp.setBorder(null);
+
+        body.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout chatTitleLayout = new javax.swing.GroupLayout(chatTitle);
+        chatTitle.setLayout(chatTitleLayout);
+        chatTitleLayout.setHorizontalGroup(
+            chatTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 727, Short.MAX_VALUE)
+        );
+        chatTitleLayout.setVerticalGroup(
+            chatTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout chatBottomLayout = new javax.swing.GroupLayout(chatBottom);
+        chatBottom.setLayout(chatBottomLayout);
+        chatBottomLayout.setHorizontalGroup(
+            chatBottomLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        chatBottomLayout.setVerticalGroup(
+            chatBottomLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 60, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout bodyLayout = new javax.swing.GroupLayout(body);
+        body.setLayout(bodyLayout);
+        bodyLayout.setHorizontalGroup(
+            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(chatTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(chatBottom, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        bodyLayout.setVerticalGroup(
+            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bodyLayout.createSequentialGroup()
+                .addComponent(chatTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 396, Short.MAX_VALUE)
+                .addComponent(chatBottom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        sp.setViewportView(body);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(sp)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(sp)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel body;
+    private component.Chat_Bottom chatBottom;
+    private component.Chat_Title chatTitle;
+    private javax.swing.JScrollPane sp;
+    // End of variables declaration//GEN-END:variables
+}
